36ccfecb5764f9b56df4106ab04153a5e34e0526
Kevin Tan <relyzer@yahoo.com>
2022-04-03 17:26:09 +0800

    Reverted next.config.js and package.json changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  assetPrefix: isProd ? 'https://github.com/relyzer/relyzer.github.io' : '',
   reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com"],
